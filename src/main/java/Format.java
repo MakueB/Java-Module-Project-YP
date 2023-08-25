cffbde6f0fa41f80c7acf68ffcc5f8e47c7880c6
MakueB <120536590+MakueB@users.noreply.github.com>
2023-08-25 18:28:18 -0400

    пытался добавить класс формат во второй ПР

--- src/main/java/Format.java
+++ src/main/java/Format.java
@@ -1,5 +1,6 @@
 public class Format {
     public static String chooseRublesEnding(double rubles){
+        //окончания для рубля
         int floorRubles = (int) Math.floor(rubles);
         if (floorRubles == 1)
             return "рубль";
