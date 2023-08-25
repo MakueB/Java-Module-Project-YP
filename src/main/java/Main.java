a30dafa0390ef025cb42c069418fc2e0a47478c4
MakueB <120536590+MakueB@users.noreply.github.com>
2023-08-24 20:11:23 -0400

    добавил проверку на отрицательную стоимость

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,13 @@
 import java.util.Scanner;
 public class Main {
+    public static int sum = 0;
+    public static String productsList = "";
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         int personsN;
-        int sum = 0;
-        String ProductsList = "";
-
         personsN = getPersonsN();
-
-        while (true){
-            Product product = addProduct();
-            if (product.name.equalsIgnoreCase("завершить")) {
-                ProductsList = ProductsList.trim();
-                break;
-            }
-            sum += product.price;
-            ProductsList += " " + product.name;
-        }
-        countSumPerPerson(personsN, sum, ProductsList);
+        addProduct();
+        countSumPerPerson(personsN, sum, productsList);
     }
     public static int getPersonsN(){
         int personsN = 0;
@@ -30,15 +20,39 @@ public class Main {
         }
         return  personsN;
     }
-    public static Product addProduct() {
-        Product good;
-        System.out.println("Введите название товара или \"завершить\", если все товары добавлены: ");
-        String name = scanner.next();
-        if (name.equalsIgnoreCase("завершить"))
-            return new Product("завершить", 0);
-        System.out.println("Введите стоимость товара: ");
-        double price = scanner.nextDouble();
-        return good = new Product(name, price);
+    public static void addProduct() {
+        Product product;
+        double price = -1;
+        String name = "";
+        while (true) {
+            while (true) {
+                System.out.println("Введите название товара или \"завершить\", если все товары добавлены: ");
+                name = scanner.next();
+                if (name.equalsIgnoreCase("завершить")) {
+                    try {
+                        if (productsList.equals(""))
+                            throw new RuntimeException();
+                        return;
+                    } catch (RuntimeException e) {
+                        System.out.println("Введите название хотя бы 1 товара");
+                    }
+                } else {
+                    productsList += " " + name;
+                    break;
+                }
+            }
+
+            while (price < 0) {
+                System.out.println("Введите стоимость товара: ");
+                price = scanner.nextDouble();
+                if (price < 0)
+                    System.out.println("Стоимость должна быть выше 0");
+            }
+            product = new Product(name, price);
+            productsList = productsList.trim();
+            sum += price;
+            price = -1;
+        }
     }
     public  static  double countSumPerPerson(int personsN, int sum, String goodsList){
         double res = (double) sum/personsN;
